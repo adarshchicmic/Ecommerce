ef682f9403ed0aedf374b7fd4eca274a8e6c2a0f
Adarsh Pandey <adarsh.pandey@chicmic.co.in>
2023-03-24 17:15:54 +0530

    deleted unwanted files

--- src/navigators/Application.js
+++ src/navigators/Application.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import {
   NavigationContainer,
   useNavigationContainerRef,
 } from '@react-navigation/native';
-import { Startup } from '../screens';
-import { useTheme } from '../hooks';
-import MainNavigator from './Main';
-import { useFlipper } from '@react-navigation/devtools';
-const Stack = createStackNavigator();
 import SignUp from '../screens/SignUp/SignUp';
 import LogIn from '../screens/Login/Login';
 import ForgotPassword from '../screens/ForgotPassword/ForgotPassword';
 import CreatePassword from '../screens/CreatePassword/CreatePassword';
-// @refresh reset
+const Stack = createStackNavigator();
+
 const ApplicationNavigator = () => {
-  const { Layout, darkMode, NavigationTheme } = useTheme();
-  const { colors } = NavigationTheme;
-  const navigationRef = useNavigationContainerRef();
-  useFlipper(navigationRef);
   return (
-    <SafeAreaView style={[Layout.fill, { backgroundColor: colors.card }]}>
-      <NavigationContainer theme={NavigationTheme} ref={navigationRef}>
-        <StatusBar barStyle={darkMode ? 'light-content' : 'dark-content'} />
+    <SafeAreaView>
+      <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="SignUp" component={SignUp} />
           <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
