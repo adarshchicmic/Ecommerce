6102382c40959519fd58b8444e56305ef94cfe11
Adarsh Pandey <adarsh.pandey@chicmic.co.in>
2023-03-27 14:24:14 +0530

    done

--- src/navigators/Application.js
+++ src/navigators/Application.js
@@ -1,37 +1,22 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import {
-  NavigationContainer,
-  useNavigationContainerRef,
-} from '@react-navigation/native';
-import { Startup } from '../screens';
-import { useTheme } from '../hooks';
-import MainNavigator from './Main';
-import { useFlipper } from '@react-navigation/devtools';
-const Stack = createStackNavigator();
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignUp from '../screens/SignUp/SignUp';
 import LogIn from '../screens/Login/Login';
 import ForgotPassword from '../screens/ForgotPassword/ForgotPassword';
 import CreatePassword from '../screens/CreatePassword/CreatePassword';
-// @refresh reset
+const Stack = createNativeStackNavigator();
+
 const ApplicationNavigator = () => {
-  const { Layout, darkMode, NavigationTheme } = useTheme();
-  const { colors } = NavigationTheme;
-  const navigationRef = useNavigationContainerRef();
-  useFlipper(navigationRef);
   return (
-    <SafeAreaView style={[Layout.fill, { backgroundColor: colors.card }]}>
-      <NavigationContainer theme={NavigationTheme} ref={navigationRef}>
-        <StatusBar barStyle={darkMode ? 'light-content' : 'dark-content'} />
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="SignUp" component={SignUp} />
-          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-          <Stack.Screen name="CreateNewPassword" component={CreatePassword} />
-          <Stack.Screen name="Login" component={LogIn} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="CreateNewPassword" component={CreatePassword} />
+        <Stack.Screen name="Login" component={LogIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 export default ApplicationNavigator;
